04b170006727c4fed88f3de109b619045d4e27d9
pajeetkumar95 <harshajeetkumar95@gmail.com>
2023-02-28 12:54:49 +0530

    tesla ecommerce

--- src/Tesla Work/Section.js
+++ src/Tesla Work/Section.js
@@ -1,16 +1,17 @@
 import React from "react";
+import Rotate from "react-reveal/Rotate"; 
 
 function Section(props) {
   return (
     <section
       style={{ backgroundImage: `url(teslaimages/${props.backgroundImage})` }}>
-      <div className="section">
-        <h1>{props.name}</h1>
-        <p>{props.para}</p>
-        <div className="anchor">
+       <div className="section">
+       <Rotate top left><h1>{props.name}</h1>
+        <p>{props.para}</p> </Rotate>
+        <Rotate right top><div className="anchor">
           {props.rightbtn && <a href="">{props.rightbtn}</a>}
           <a href="">{props.leftbtn}</a>
-        </div>
+        </div></Rotate>
         <div className="image">
           {props.icon && <img className="icon" src="teslaimages/down-arrow.svg"></img>}
           </div>
